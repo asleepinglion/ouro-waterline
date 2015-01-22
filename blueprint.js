814d6478bd03fb426b5c583faaed240d2e881832
Aaron Storck <aaronstorck@gmail.com>
2015-01-21 19:20:06 -0500

    Updated the blueprint for crud methods.

--- blueprint.js
+++ blueprint.js
@@ -6,29 +6,26 @@ module.exports = {
 
     search: {
 
-      security: false,
-      description: "Search the {{model}} database...",
+      description: "Search the database records...",
 
       params: {
 
         where: {
-
           description: 'The where parameter allows you to filter records using the Waterline query language.',
+          type: 'object',
+          default: {},
 
           transform: {
             json: true
           },
-
           validate: {
-            //waterlineQuery: true, //test to make sure the parameter is valid waterline query language.
-            required: true,
             json: true
-            //model: true //run model validations against the parameter contents.
           }
         },
 
         sort: {
           description: 'The sort parameter allows you to sort records using standard SQL notation (e.g. field ASC).',
+          type: 'string',
           validate: {
             sortAttribute: true, //validate sort attribute
             sortDirection: true //validate the sort direction
@@ -36,16 +33,21 @@ module.exports = {
         },
 
         limit: {
-          description: 'The limit parameter allows you specify the number of results to return.'
+          description: 'The limit parameter allows you specify the number of results to return.',
+          type: 'integer',
+          default: 25,
+          validate: {
+            min: 0,
+            max: 1000
+          }
         },
 
         skip: {
-          description: 'The skip parameter allows you to page results in conjunction with the limit parameter.'
-        },
-
-        test: {
+          description: 'The skip parameter allows you to page results in conjunction with the limit parameter.',
+          type: 'integer',
+          default: 0,
           validate: {
-            required: true
+            min: 0
           }
         }
 
@@ -55,25 +57,53 @@ module.exports = {
 
     create: {
 
-      description: "Create a new {{model}} record...",
+      description: "Create a new database record...",
 
       params: {
 
         attributes: {
           description: 'The attributes for the record you wish to create.',
           type: 'object',
-
-          validate: {
-            model: true
+          transform: {
+            json: true
           },
+          model: true
+        }
 
-          sanitize: {
-            model: true
-          }
+      }
+    },
 
+    update: {
+
+      description: "Update a database record...",
+
+      params: {
+
+        attributes: {
+          description: 'The attributes for the record you wish to update.',
+          type: 'object',
+          transform: {
+            json: true
+          },
+          model: true
+        }
+
+      }
+    },
+
+    delete: {
+
+      description: "Delete a new database record...",
+
+      params: {
+
+        id: {
+          description: 'The attributes for the record you wish to delete.',
+          type: 'integer'
         }
 
       }
     }
+
   }
 };
