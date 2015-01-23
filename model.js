d52741d862c2838ee680448c495a18401fdde283
Aaron Storck <aaronstorck@gmail.com>
2015-01-22 21:29:24 -0500

    Updated the model class to wrap the necessary functions from the underlying waterline engine.

--- model.js
+++ model.js
@@ -13,5 +13,24 @@ var SuperJS = require('superjs');
 
 module.exports = SuperJS.Model.extend({
 
+  find: function(criteria) {
+    return this.engine.find(criteria);
+  },
+
+  findOne: function(criteria) {
+    return this.engine.findOne(criteria);
+  },
+
+  create: function(criteria) {
+    return this.engine.create(criteria);
+  },
+
+  update: function(searchCriteria, values) {
+    return this.engine.update(searchCriteria, values);
+  },
+
+  destroy: function(criteria) {
+    return this.engine.destroy(criteria);
+  }
 
 });
